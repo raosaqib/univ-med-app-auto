4185151cf32b141a6d2533800dfe9a16a9ef5e8d
raosaqib <rao.saqib@hotmail.com>
2020-10-09 22:22:47 +0300

    Update UniversalMediaPlayerLandingpage.java

--- src/main/java/com/gk/univmed/auto/pages/UniversalMediaPlayerLandingpage.java
+++ src/main/java/com/gk/univmed/auto/pages/UniversalMediaPlayerLandingpage.java
@@ -9,18 +9,15 @@ public class UniversalMediaPlayerLandingpage extends AppiumBase {
 	boolean status;
 	@AndroidFindBy(accessibility = "Open the main menu")
 	private MobileElement landPage;
-		@Step("user should navigate to home page")
+	@Step("user should navigate to home page")
 	public boolean userTapsOnHomePage() {
 		status = super.click(landPage, "press login button");
 		return status;
-
 	}
 	@Step("validate landing page text")
 	public String getlandingPageText() {
 		MobileElement landingPageText = (MobileElement) driver.findElementById("com.instantappsample.uamp:id/toolbar");
 		String txt = landingPageText.getText();
 		return txt;
-
 	}
-
 }
